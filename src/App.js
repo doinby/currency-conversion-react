967895282a317c26ca93fdc6107177820dbecf08
Chinh Do <do.inby@gmail.com>
2021-11-09 11:57:42 +1100

    added gitignore. Updated header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+/* eslint-disable no-unused-vars */
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Header from './components/Header';
 
-function App() {
-  return <>App works!</>;
+const baseUrl =
+  'https://v6.exchangerate-api.com/v6/9f68f70f705cfe734fe1e1a9/latest/USD';
+
+export default function App() {
+  useEffect(() => {
+    fetch(baseUrl)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
+
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
-
-export default App;
