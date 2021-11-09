0a6c29d274d8407c1d2e420eda1cc6dd74f56d73
Chinh Do <do.inby@gmail.com>
2021-11-09 21:11:21 +1100

    added currency options in a select list. onChange option works

--- src/components/CurrencyInput.js
+++ src/components/CurrencyInput.js
@@ -1,15 +1,31 @@
 import React from 'react';
 
-export default function CurrencyInput(props) {
+export default function CurrencyInput({
+  name,
+  defaultValue,
+  onChangeOption,
+  currencyOptions,
+}) {
+  const defaultIndex = currencyOptions.indexOf(defaultValue);
+
   return (
-    <div>
-      <label>Convert {props.name}:</label>
+    <div className='input-container'>
+      <label>Convert {name}:</label>
       <br />
-      <input type='text' name={props.name} />
-      <select>
-        <option value='currency 1'>currency 1</option>
-        <option value='currency 2'>currency 2</option>
-        <option value='currency 3'>currency 3</option>
+      <input type='text' name={name} />
+      <select name={name} onChange={onChangeOption}>
+        <option key={defaultIndex} value={defaultValue}>
+          {defaultValue}
+        </option>
+        {currencyOptions.map((option, index) => {
+          if (index !== defaultIndex) {
+            return (
+              <option key={index} value={option}>
+                {option}
+              </option>
+            );
+          } else return null;
+        })}
       </select>
     </div>
   );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 import React, {useState, useEffect} from 'react';
 import CurrencyInput from './CurrencyInput';
@@ -6,17 +7,40 @@ const baseUrl =
   'https://v6.exchangerate-api.com/v6/9f68f70f705cfe734fe1e1a9/latest/USD';
 
 export default function Main() {
+  const [currencyOptions, setCurrencyOptions] = useState([]);
+  const [fromCurrency, setFromCurrency] = useState('AUD');
+  const [toCurrency, setToCurrency] = useState('USD');
+
   useEffect(() => {
     fetch(baseUrl)
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        const currencies = Object.keys(data.conversion_rates);
+
+        setCurrencyOptions(currencies);
+      });
   }, []);
+
+  function handleChangedOption(event) {
+    const changedCurrency = event.target.value;
+    return changedCurrency;
+  }
 
   return (
     <main>
-      <form className='converter'>
-        <CurrencyInput name='from' />
-        <CurrencyInput name='to' />
+      <form className='converter-form'>
+        <CurrencyInput
+          name='from'
+          onChangeOption={handleChangedOption}
+          defaultValue={fromCurrency}
+          currencyOptions={currencyOptions}
+        />
+        <CurrencyInput
+          name='to'
+          onChangeOption={handleChangedOption}
+          defaultValue={toCurrency}
+          currencyOptions={currencyOptions}
+        />
       </form>
     </main>
   );
