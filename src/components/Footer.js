5dc58c69023c43931656f9b8bec9a22ff9381be0
Chinh <do.inby@gmail.com>
2021-11-24 11:29:30 +1100

    update github link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,5 +2,7 @@ import React from 'react';
 import {githubIcon} from './icons';
 
 export default function Footer() {
-  return <footer>{githubIcon}</footer>;
+  return <footer>
+    <a href='github.com/doinby'>{githubIcon}</a>
+  </footer>;
 }
